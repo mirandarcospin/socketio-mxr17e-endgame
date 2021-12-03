f71a226bd15abd7689274c4f87c0a7b50efe2ed0
Miranda Ramirez Cospin <42848800+mirandarcospin@users.noreply.github.com>
2021-12-03 14:05:18 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-const express = require('express');
-const app = express();
+const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const path = require('path');
+// const path = require('path');
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
