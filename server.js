3c4942c867980afa538521c9c08241629243ceac
Miranda Ramirez Cospin <42848800+mirandarcospin@users.noreply.github.com>
2021-12-05 20:40:06 -0600

    Update server.js

--- server.js
+++ server.js
@@ -7,38 +7,11 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', (socket) => {
-  socket.on('forCorrectParentNode', (data) => {
-    io.emit('forCorrectParentNode', data);
-  });
-
-  socket.on('forLettersParentNode', (data) => {
-    io.emit('forLettersParentNode', data);
-  });
-
-  socket.on('forContext', (d1,d2,d3,d4) => {
-    io.emit('forContext', d1,d2,d3,d4);
-  });
-
-  socket.on('forLettersParentNode', (data) => {
-    io.emit('forLettersParentNode', data);
-  });
-
-  socket.on('gameHangmanP1', (data) => {
-    io.emit('gameHangmanP1', data);
-  });
-
-  socket.on('gameHangmanP2', (data) => {
-    io.emit('gameHangmanP2', data);
-  });
-
-  socket.on('forGamePoints', (data) => {
-    io.emit('forGamePoints', data);
-  });
-
-  socket.on('gamePlay', () => {
-    io.emit('gamePlay');
-  });  
+io.on('connection', function(socket){
+   console.log('A user connected');
+   socket.on('disconnect', function () {
+      console.log('A user disconnected');
+   });
 });
 
 http.listen(port, () => {
