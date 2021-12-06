1c9ff25579ae34150b823d693aa096b720400bc6
Miranda Ramirez Cospin <42848800+mirandarcospin@users.noreply.github.com>
2021-12-05 19:57:42 -0600

    Update server.js

--- server.js
+++ server.js
@@ -8,13 +8,37 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
+  socket.on('forCorrectParentNode', (data) => {
+    io.emit('forCorrectParentNode', data);
+  });
+
+  socket.on('forLettersParentNode', (data) => {
+    io.emit('forLettersParentNode', data);
+  });
+
+  socket.on('forContext', (d1,d2,d3,d4) => {
+    io.emit('forContext', d1,d2,d3,d4);
+  });
+
+  socket.on('forLettersParentNode', (data) => {
+    io.emit('forLettersParentNode', data);
+  });
+
   socket.on('gameHangmanP1', (data) => {
-    io.emit('gameHangmanp1', data);
+    io.emit('gameHangmanP1', data);
   });
 
   socket.on('gameHangmanP2', (data) => {
     io.emit('gameHangmanP2', data);
   });
+
+  socket.on('forGamePoints', (data) => {
+    io.emit('forGamePoints', data);
+  });
+
+  socket.on('gamePlay', () => {
+    io.emit('gamePlay');
+  });  
 });
 
 http.listen(port, () => {
