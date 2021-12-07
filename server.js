d7049a6c90a8ecaeedc60467af54aefcc1bf78aa
Miranda Ramirez Cospin <42848800+mirandarcospin@users.noreply.github.com>
2021-12-07 14:38:21 -0600

    Update server.js

--- server.js
+++ server.js
@@ -7,13 +7,32 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-var players = new Map();
+// var players = new Map();
+// var playerID = 0;
+// io.on('connection', (socket) => {
+//   playerID += 1;
+//   console.log('Socket onnection ' + socket.id + ' and Player # ' + playerID);
+//   players.set(socket.id, playerID);
+//   socket.emit('forID', playerID);
+// });
+
 var playerID = 0;
-io.on('connection', (socket) => {
+io.on('connection', function(socket){
   playerID += 1;
-  console.log('Socket onnection ' + socket.id + ' and Player # ' + playerID);
-  players.set(socket.id, playerID);
-  socket.emit('forID', playerID);
+  console.log('connection ' + socket.id + ' playerId ' + playerID);
+  socket.emit('yourid', playerID);
+  socket.on('disconnect', function () {
+    playerID--;
+  });
+  socket.on('player1Points',(data) => {
+    io.emit('player1Points',(data));
+  });
+  socket.on('player2Points',(data) => {
+    io.emit('player2Points',(data));
+  });
+  socket.on('gamePoints',(data) => {
+    io.emit('gamePoints',(data));
+  });
 });
 
 http.listen(port, () => {
