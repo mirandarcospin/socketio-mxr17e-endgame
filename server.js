0f4d503d993796babc3a5d185369ff5932bacff1
Miranda Ramirez Cospin <42848800+mirandarcospin@users.noreply.github.com>
2021-12-07 21:56:56 -0600

    Update server.js

--- server.js
+++ server.js
@@ -7,34 +7,38 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-// var players = new Map();
-// var playerID = 0;
-// io.on('connection', (socket) => {
-//   playerID += 1;
-//   console.log('Socket onnection ' + socket.id + ' and Player # ' + playerID);
-//   players.set(socket.id, playerID);
-//   socket.emit('forID', playerID);
-// });
-
 var playerID = 0;
 io.on('connection', function(socket){
   playerID += 1;
   console.log('connection ' + socket.id + ' playerId ' + playerID);
   socket.emit('yourid', playerID);
 
-  socket.on('disconnect', function (socket) {
-    playerID -= 1;
-  });
-
-  socket.on('player1Points', data => {
-    io.emit('player1Points', data);
-  });
-  socket.on('player2Points', data => {
-    io.emit('player2Points', data);
-  });
+  if(playerID % 2 != 0) {
+    socket.on('player1Points', data => {
+      io.emit('player1Points', data);
+    });
+  }
+  else if(playerID % 2 == 0){
+    socket.on('player2Points', data => {
+      io.emit('player2Points', data);
+    });
+  }
   socket.on('gamePoints', data => {
     io.emit('gamePoints', data);
   });
+  
+  socket.on('disconnect', function (socket) {
+    playerID -= 1;
+  });
+//   socket.on('player1Points', data => {
+//     io.emit('player1Points', data);
+//   });
+//   socket.on('player2Points', data => {
+//     io.emit('player2Points', data);
+//   });
+//   socket.on('gamePoints', data => {
+//     io.emit('gamePoints', data);
+//   });
 });
 
 http.listen(port, () => {
